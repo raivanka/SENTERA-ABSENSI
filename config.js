591bcef24d9f964ff9305913f9f70742395c20cb
Neil <raivanka7@gmail.com>
2026-09-13 18:59:39 +0700

    feat: migrasi backend ke PHP InfinityFree

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzUNayLthLLRwfms4CQHTqTKj3lKhyrhWwb7mOIbMHEIM4HkC0jbwVgy-MYJaV1WxNXcQ/exec";
+const API_URL = "http://sentera-absensi.infinityfreeapp.com/api.php";
